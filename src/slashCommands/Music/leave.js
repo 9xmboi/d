7e5f9199a32459796ac609cda88a1330d7b355d9
Code-Killer <83761474+Code-Killer-0001@users.noreply.github.com>
2022-01-21 10:36:20 +0530

    Update leave.js

--- src/slashCommands/Music/leave.js
+++ src/slashCommands/Music/leave.js
@@ -14,8 +14,8 @@ module.exports = {
     await interaction.deferReply({
       ephemeral: false
     });
-      if(!interaction.member.voice.channel) return interaction.editReply({embeds: [new MessageEmbed ().setColor('#ff0000').setDescription("You are not connect in vc")]});
-      if(interaction.guild.me.voice.channel && interaction.guild.me.voice.channelId !== interaction.member.voice.channelId) return interaction.editReply({embeds: [new MessageEmbed ().setColor('#ff0000').setDescription(`You are not connected to <#${interaction.guild.me.voice.channelId}> to use this command.`)]});
+      if(!interaction.member.voice.channel) return interaction.editReply({embeds: [new MessageEmbed ().setColor(client.embedColor).setDescription("You are not connect in vc")]});
+      if(interaction.guild.me.voice.channel && interaction.guild.me.voice.channelId !== interaction.member.voice.channelId) return interaction.editReply({embeds: [new MessageEmbed ().setColor(client.embedColor).setDescription(`You are not connected to <#${interaction.guild.me.voice.channelId}> to use this command.`)]});
 
     const player = client.manager.get(interaction.guildId);
    
@@ -24,8 +24,8 @@ module.exports = {
         player.destroy();
         
         let thing = new MessageEmbed()
-          .setColor('#ff0000')
-          .setDescription(`<a:tick:919227918738133022>** | Leave the voice channel**\nThank you for using ${message.client.user.username}!`)
+          .setColor(client.embedColor)
+          .setDescription(`<a:tick:919227918738133022>** | Leave the voice channel**\nThank you for using ${interaction.client.user.username}!`)
         return interaction.editReply({ embeds: [thing] });
         
         }
