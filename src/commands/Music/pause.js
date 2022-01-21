6f8b34e802b5566e7bb0d23d7bf1d7dc04564ba7
Code-Killer <83761474+Code-Killer-0001@users.noreply.github.com>
2022-01-21 10:43:48 +0530

    Update pause.js

--- src/commands/Music/pause.js
+++ src/commands/Music/pause.js
@@ -1,35 +1,40 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, CommandInteraction, Client } = require("discord.js")
 
 module.exports = {
     name: "pause",
-    category: "Music",
     description: "Pause the currently playing music",
-    args: false,
-    usage: "",
-    permission: [],
-    owner: false,
-    player: true,
-    inVoiceChannel: true,
-    sameVoiceChannel: true,
- execute: async (message, args, client, prefix) => {
-    
-		const player = message.client.manager.get(message.guild.id);
+
+	
+    /**
+     * 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     */
+
+    run: async (client, interaction) => {
+        await interaction.deferReply({
+          ephemeral: false
+        });
+      if(!interaction.member.voice.channel) return interaction.editReply({embeds: [new MessageEmbed ().setColor(client.embedColor).setDescription("You are not connect in vc")]});
+      if(interaction.guild.me.voice.channel && interaction.guild.me.voice.channelId !== interaction.member.voice.channelId) return interaction.editReply({embeds: [new MessageEmbed ().setColor(client.embedColor).setDescription(`You are not connected to <#${interaction.guild.me.voice.channelId}> to use this command.`)]});
+
+        const player = interaction.client.manager.get(interaction.guildId);
 
         if (!player.queue.current) {
             let thing = new MessageEmbed()
-                .setColor('#ff0000')
+                .setColor("RED")
                 .setDescription("There is no music playing.");
-            return message.reply({embeds: [thing]});
+            return interaction.editReply({embeds: [thing]});
         }
 
         const emojipause = client.emoji.pause;
 
         if (player.paused) {
             let thing = new MessageEmbed()
-                .setColor('#ff0000')
-                .setDescription(`<a:stop:919229116136448020>** | The player is already paused.**`)
+                .setColor("RED")
+                .setDescription(`${emojipause} The player is already paused.`)
                 .setTimestamp()
-                return message.reply({embeds: [thing]});
+                return interaction.editReply({embeds: [thing]});
         }
 
         player.pause(true);
@@ -37,10 +42,12 @@ module.exports = {
         const song = player.queue.current;
 
         let thing = new MessageEmbed()
-            .setColor('#ff0000')
+            .setColor(client.embedColor)
             .setTimestamp()
             .setDescription(`<:pause:919236040470966273>** | Paused**\n[${song.title}](${song.uri})`)
-          return message.reply({embeds: [thing]});
+          return interaction.editReply({embeds: [thing]});
 	
     }
 };
+     
+
