17c7e9b6a1a46c35a9eea82274d2564bdcf9deb4
Code-Killer <83761474+Code-Killer-0001@users.noreply.github.com>
2022-01-29 09:19:53 +0530

    Update messageCreate.js

--- src/events/Client/messageCreate.js
+++ src/events/Client/messageCreate.js
@@ -1,29 +1,28 @@
 const { MessageEmbed, Permissions } = require("discord.js");
-const pre= require("../../schema/prefix.js");
+const db = require("../../schema/prefix.js");
 
-module.exports = async (client, message) => {
+module.exports = {
+    name: "messageCreate",
+    run: async (client, message) => {
    
    if (message.author.bot) return;
    if (!message.guild) return;
     let prefix = client.prefix;
     const channel = message?.channel;
-    const ress =  await pre.findOne({guildid: message.guild.id})
-   if(ress && ress.prefix)prefix = ress.prefix;
-   
+    const ress =  await db.findOne({Guild: message.guildId})
+   if(ress && ress.Prefix)prefix = ress.Prefix;
+
     const mention = new RegExp(`^<@!?${client.user.id}>( |)$`);
     if (message.content.match(mention)) {
       const embed = new MessageEmbed()
-        .setColor('#ff0000')
+        .setColor(client.embedColor)
         .setDescription(`**› My prefix in this server is \`${prefix}\`**\n**› You can see my all commands type \`${prefix}\`help**`);
       message.channel.send({embeds: [embed]})
     };
-   const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-
+    const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
     const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
     if (!prefixRegex.test(message.content)) return;
-
     const [ matchedPrefix ] = message.content.match(prefixRegex);
-
     const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
@@ -38,7 +37,7 @@ module.exports = async (client, message) => {
     if(!message.guild.me.permissions.has(Permissions.FLAGS.EMBED_LINKS)) return await message.channel.send({ content: `I don't have **\`EMBED_LINKS\`** permission to execute this **\`${command.name}\`** command.` }).catch(() => {});
     
     const embed = new MessageEmbed()
-        .setColor('#ff0000');
+        .setColor("RED");
 
     // args: true,
     if (command.args && !args.length) {
@@ -57,11 +56,11 @@ module.exports = async (client, message) => {
         embed.setDescription("You can't use this command.");
         return message.channel.send({embeds: [embed]});
     }
-   if (!channel.permissionsFor(message.guild.me)?.has(Permissions.FLAGS.EMBED_LINKS) && client.user.id !== user.id) {
+   if (!channel.permissionsFor(message.guild.me)?.has(Permissions.FLAGS.EMBED_LINKS) && client.user.id !== userId) {
         return channel.send({ content: `Error: I need \`EMBED_LINKS\` permission to work.` });
       }
     if (command.owner && message.author.id !== `${client.owner}`) {
-        embed.setDescription("Only <@531063766645997570> can use this command!");
+        embed.setDescription("Only <@933415227184263189> can use this command!");
         return message.channel.send({embeds: [embed]});
     }
 
@@ -94,3 +93,4 @@ module.exports = async (client, message) => {
         return message.channel.send({embeds: [embed]});
     }
   }
+};
